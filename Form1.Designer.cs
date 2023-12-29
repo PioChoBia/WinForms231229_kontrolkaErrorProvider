88965b00529d31ca5cbcfe6bdb4c43fe79dcc3ed
Pio <pio.cho.bia@gmail.com>
2023-12-29 09:45:44 +0100

    wersja 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelKomunikat = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(163, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 96);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "testy\r\nz kontolką\r\nErrorProvider\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 185);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "wprowadź liczbę";
+            // 
+            // labelKomunikat
+            // 
+            this.labelKomunikat.AutoSize = true;
+            this.labelKomunikat.Location = new System.Drawing.Point(357, 188);
+            this.labelKomunikat.Name = "labelKomunikat";
+            this.labelKomunikat.Size = new System.Drawing.Size(12, 15);
+            this.labelKomunikat.TabIndex = 4;
+            this.labelKomunikat.Text = "_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(553, 450);
+            this.Controls.Add(this.labelKomunikat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private ErrorProvider errorProvider1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label labelKomunikat;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,33 @@ namespace WinForms231229_kontrolkaErrorProvider
 {
     public partial class Form1 : Form
     {
+        public ErrorProvider errorProviderMoja = new ErrorProvider();
+
         public Form1()
         {
             InitializeComponent();
+                      
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            errorProviderMoja.Clear();
+            labelKomunikat.Text = "";
+            foreach (char c in textBox1.Text)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    errorProviderMoja.SetError(textBox1, "znak: "+c+" nie jest cyfr¹!");
+                    labelKomunikat.Text = errorProviderMoja.GetError(textBox1);
+                    break;
+                }
+
+            }
+
+            
         }
     }
 }
